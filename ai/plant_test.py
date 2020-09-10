2750c3162cf990c80f6ebc81810e9d9d76aa978c
이윤민 <dbsalsdl5@naver.com>
2020-09-10 20:32:32 +0900

    S03P22D207-15 | xml을 csv로 저장 성공

--- ai/plant_test.py
+++ ai/plant_test.py
@@ -1,16 +1,38 @@
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
 from urllib.parse import quote_plus, unquote
+import os
+from bs4 import BeautifulSoup
+import pandas as pd
+from time import sleep
 
-url = 'http://openapi.forest.go.kr/openapi/service/cultureInfoService/fStoryOpenAPI'
-# queryParams = '?' + urlencode({ quote_plus('ServiceKey') : 'iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D' })
-# key가 인코딩 되어있어서 디코딩 해주기
-decode_key = unquote('iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D')
-queryParams = '?' + urlencode({ quote_plus('ServiceKey') : decode_key })
 
-request = Request(url + queryParams)
-request.get_method = lambda: 'GET'
-# response_body = urlopen(request).read()
-# 결과가 인코딩 되어있어서 디코딩 해주기
-response_body = urlopen(request).read().decode('utf-8')
-print(response_body)
+############### print와 sleep이 있어야 500에러 안 뜸. 서버에 너무 많이 계속 접근하면 500에러 떠서 중간에 딜레이 줘야함
+
+pageNO = [x for x in range(1,19)] 
+df = pd.DataFrame(columns=['fslistno', 'fskname', 'fsinhabit', 'fsguide', 'fslifetime', 'imgfilename1', 'imgfilename2'])
+k = 0
+for i in range(len(pageNO)):
+    URL = 'http://apis.data.go.kr/openapi/service/cultureInfoService/fStoryOpenAPI?serviceKey=iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D&type=xml&pageNo='+str(pageNO[i])+'&numOfRows=10&flag=Y'
+    
+    # if i!=0:
+    #     sleep(200) 
+    data = urlopen(URL).read()
+    soup = BeautifulSoup(data, "html.parser")
+    print("here!!!!!")
+    for item in soup.findAll("item"):
+        imageName = ['', '']
+        imageURL = 'http://apis.data.go.kr/openapi/service/cultureInfoService/fStoryImgOpenAPI?serviceKey=iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D&searchWrd='+item.fslistno.text
+        imageData = urlopen(imageURL).read()
+        imageSoup = BeautifulSoup(imageData, "html.parser")
+        idx = 0
+        for image in imageSoup.findAll("item"):
+            imageName[idx] = 'http://www.forest.go.kr/images/data/down/story/' + image.imgfilename.text
+            print(item.fslistno.text, ' ', image.imgno.text , ' ', idx, ' ', imageName[idx])
+            idx = idx + 1
+            idx = idx % 2
+            sleep(0.01)
+
+        df.loc[k] = [item.fslistno.text, item.fskname.text, item.fsinhabit.text, item.fsguide.text, item.fslifetime.text, imageName[0], imageName[1]]
+        k=k+1
+df.to_csv('./plantList.csv',encoding='utf-8-sig')
