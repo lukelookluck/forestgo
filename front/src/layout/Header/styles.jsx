4c25e98ac06db63e9f18db2662082e165994e3e8
이윤민 <dbsalsdl5@naver.com>
2020-09-15 15:02:27 +0900

    S03P22D207-24 | 필요한 파일 폴더 구성

---
+++ front/src/layout/Header/styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    text-align: center;
+    & .logoGrid {
+        height: 100px;
+        background-color: rgb(56, 97, 149);
+        color: white;
+    }
+
+    & #logo {
+        margin-top: 30px;
+        font-size: 30px;
+    }
+`;
+
+export default Wrapper;
---
+++ front/src/layout/NavigationTab/index.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import Wrapper from "./styles";
+import { Grid } from "@material-ui/core";
+
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Community from "../../pages/Community/";
+import MyForest from "../../pages/MyForest/";
+
+import CreateArticleButton from "../../components/Community/ArticleForm/CreateArticleButton/";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={2}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function NavigationTab() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  return (
+    <Wrapper className={classes.root}>
+      <Grid container justify="center" alignItems="center" class="asd">
+        <AppBar className="myAppbar" position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+            className="navTabs"
+          >
+            <Tab
+              label="Discovery"
+              {...a11yProps(0)}
+            />
+            <Tab
+              label="MyForest"
+              {...a11yProps(1)}
+            />
+          </Tabs>
+        </AppBar>
+        <SwipeableViews
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+          index={value}
+          onChangeIndex={handleChangeIndex}
+          class="asdas"
+        >
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <Community></Community>
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <MyBar></MyBar>
+          </TabPanel>
+        </SwipeableViews>
+        {value == 0 && <CreateArticleButton />}
+      </Grid>
+    </Wrapper>
+  );
+}
