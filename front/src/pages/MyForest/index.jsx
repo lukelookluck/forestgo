6d5dff1e095629da310f0f65b7f864417b6b5716
이윤민 <dbsalsdl5@naver.com>
2020-09-15 14:43:42 +0900

    S03P22D207-11 | 기본폴더, 라우터 생성

---
+++ front/src/pages/MyForest/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Wrapper from "./styles";
+import { Grid } from "@material-ui/core";
+
+
+const MyForest = () => {
+
+  return (
+    <Wrapper>
+      <Grid>
+        <h1>My Forest</h1>
+      </Grid>
+    </Wrapper>
+  );
+};
+
+export default MyForest;
