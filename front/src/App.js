3586cdd5741d4468d790fbd8e44f46d6b302cdf4
Luke <gunhee9177@naver.com>
2020-09-16 09:29:38 +0900

    a

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Start from "./pages/Start/";
+import SignUp from "./pages/SignUp/";
+import FindPW from "./pages/FindPW/";
 import MyForest from "./pages/MyForest/";
 import Main from "./pages/Main/";
 import Discovery from "./pages/Discovery/";
@@ -12,6 +14,8 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Start}></Route>
+        <Route exact path="/signup" component={SignUp}></Route>
+        <Route exact path="/findpW" component={FindPW}></Route>
         <Route exact path="/Main" component={Main}></Route>
         <Route exact path="/MyForest" component={MyForest}></Route>
         <Route exact path="/Discovery" component={Discovery}></Route>
