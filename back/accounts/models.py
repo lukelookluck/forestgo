67507aadfbf8793be16819b1722ee58c06d4e962
yehee <yveltal@naver.com>
2020-09-16 15:52:56 +0900

    S03P22D207-17 | added model

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Userinfo(models.Model):
+    email = models.CharField(max_length=50)
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+    profile_img = models.CharField(max_length=100)
+    registered_at = models.DateTimeField(auto_now_add=True)
