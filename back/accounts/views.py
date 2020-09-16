28fb04813ccd12b037743e55106e3b43f3f251da
yehee <yveltal@naver.com>
2020-09-16 17:52:36 +0900

    S03P22D207-17 | added serializer, GET

--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from django.http.response import JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .models import Userinfo
+from .serializers import UserinfoSerializer
+
+# rest framework R
+@api_view(['GET'])
+def userinfo_list(request):
+    userinfos = Userinfo.objects.all()
+    serializer = UserinfoSerializer(userinfos, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def userinfo_detail(request, userinfo_pk):
+    userinfo = get_object_or_404(Userinfo, pk=userinfo_pk)
+    serializer = UserinfoSerializer(userinfo)
+    return Response(serializer.data)
+
+
+
+
+
+#--- 아래는 legacy
+
+# 노가다
+@require_GET
+def userinfo_list_json(request):
+    userinfos = Userinfo.objects.all()
+
+    data = []
+    for userinfo in userinfos:
+        data.append({
+            'user_id': userinfo.id,
+            'user_email': userinfo.email,
+            'user_name': userinfo.name,
+            'user_password': userinfo.password,
+            'user_profile_img' : userinfo.profile_img,
+            'user_registered_at' : userinfo.registered_at,
+        })
+    
+    return JsonResponse(data, safe=False)
+
+# django core serializer
+@require_GET
+def userinfo_list_json2(request):
+    from django.core import serializers
+
+    userinfos = Userinfo.objects.all()
+    data = serializers.serialize('json', userinfos)
+
+    return HttpResponse(data, content_type='application/json')
+
