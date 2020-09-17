a532b70865f3132e1b5f2f654fc27f016dfc4610
yehee <yveltal@naver.com>
2020-09-17 15:47:04 +0900

    S03P22D207-18 | added forestbook, challenge models

--- back/accounts/admin.py
+++ back/accounts/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from accounts.models import Userinfo
+from challenge.models import Challenge, Userchallenge
+from forestbook.models import Forestbook, Userbook
 
 
 admin.site.register(Userinfo, UserAdmin)
+admin.site.register(Challenge)
+admin.site.register(Userchallenge)
+admin.site.register(Forestbook)
+admin.site.register(Userbook)
--- back/forestbook/models.py
+++ back/forestbook/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
-class forestbook(models.Model):
+class Forestbook(models.Model):
     name = models.CharField('NAME', max_length=20)
     fullname = models.CharField('FULLNAME', max_length=100)
     species = models.CharField('SPECIES', max_length=20)
@@ -17,10 +18,12 @@ class forestbook(models.Model):
     def get_absolute_url(self):
         pass
 
-class userbook(models.Model):
-    registered_at = models.DateTimeField('REGISTERED AT', auto_now_add=True)
+class Userbook(models.Model):
+    userinfo_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='', blank=True, null=True)
+    forestbook_id = models.ForeignKey(Forestbook, on_delete=models.CASCADE)
+    date = models.DateTimeField('DATE', auto_now_add=True)
     img = models.CharField('IMG', max_length=100, blank=True)
-
+    
     def __str__(self):
         pass
     
