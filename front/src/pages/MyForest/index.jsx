4ad3b9d38507227c8924d1599ef4e718026f8de5
이윤민 <dbsalsdl5@naver.com>
2020-09-17 17:46:10 +0900

    S03P22D207-11 | update medal

--- front/src/pages/MyForest/index.jsx
+++ front/src/pages/MyForest/index.jsx
@@ -1,31 +1,50 @@
 import React from "react";
 import Wrapper from "./styles";
 import { Grid } from "@material-ui/core";
+import Paper from '@material-ui/core/Paper';
 
 const MyForest = () => {
   return (
     <Wrapper>
       <Grid container className="root" justify="center" alignItems="center">
-        <Grid item xs={12} className="level">등급</Grid>
-        <Grid item xs={12}>
-          <div>등급에 맞는 이미지</div>
+
+        <Grid item xs={12} className="papergrid">
+          <Paper variant="outlined">
+            <Grid className="title">등급</Grid>
+            <img src="/images/bronze.png" classNmae="medal" width="100px"></img>
+            <Grid className="medal">브론즈</Grid>
+          </Paper>
         </Grid>
-        <Grid item xs={12} className="level">발견한 식물 수</Grid>
-        <Grid item xs={12}>
-          <div>N 개</div>
+
+        <Grid item xs={12} className="papergrid">
+          <Paper variant="outlined">
+            <Grid className="title">발견한 식물 수</Grid>
+            <Grid className="count">9 개</Grid>
+          </Paper>
         </Grid>
-        <Grid item xs={12} className="level">서식지</Grid>
-        <Grid item xs={12}>
-          <div>육상 : N 개</div>
-          <div>수상 : N 개</div>
+
+        <Grid item xs={12} className="papergrid">
+          <Paper variant="outlined">
+            <Grid className="title">서식지</Grid>
+            <Grid container justify="center" alignItems="center">
+              <Grid item xs={6} className="habi">육상 : 7개</Grid>
+              <Grid item xs={6} className="habi">수상 : 2개</Grid>
+            </Grid>
+          </Paper>
         </Grid>
-        <Grid item xs={12} className="level">계절</Grid>
-        <Grid item xs={12}>
-          <div>봄 : N 개</div>
-          <div>여름 : N 개</div>
-          <div>가을 : N 개</div>
-          <div>겨울 : N 개</div>
+
+        <Grid item xs={12} className="papergrid">
+          <Paper variant="outlined">
+            <Grid className="title">계절</Grid>
+            <Grid item xs={12}>
+              <div>봄 : N 개</div>
+              <div>여름 : N 개</div>
+              <div>가을 : N 개</div>
+              <div>겨울 : N 개</div>
+            </Grid>
+          </Paper>
         </Grid>
+
       </Grid>
     </Wrapper>
   );
--- front/src/pages/MyForest/styles.jsx
+++ front/src/pages/MyForest/styles.jsx
@@ -2,11 +2,36 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
 
-    .level {
+    .title {
         text-align: left;
+        font-size: 23px;
+        font-weight: bold;
+        margin-left: 13px;
+        margin-top: 10px;
+    }
+
+    .medal {
+        padding-top: 10px;
+        padding-bottom: 10px;
         font-size: 20px;
     }
 
+    .count {
+        padding-top: 10px;
+        padding-bottom: 10px;
+        font-size: 20px;
+    }
+
+    .habi {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        font-size: 20px;
+    }
+
+    .papergrid {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
 
 `;
 
