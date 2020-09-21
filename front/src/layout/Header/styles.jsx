6aa7185293d463bf2c10da7e7885abc1efa5b4c5
이윤민 <dbsalsdl5@naver.com>
2020-09-21 17:57:20 +0900

    S03P23D207-5 | Header, 오타 수정

--- front/src/layout/Header/styles.jsx
+++ front/src/layout/Header/styles.jsx
@@ -1,16 +1,30 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-    text-align: center;
-    & .logoGrid {
-        height: 100px;
-        background-color: #509c82;
-        color: white;
-    }
+  text-align: center;
+  & .logoGrid {
+      height: 100px;
+      width: 63%;
+      background-color: #509c82;
+      color: white;
+  }
+
+  & .imgGrid {
+    background-color: #509c82;
+    height: 100px;
+    width: 37%;
+  }
+
+  & #imglogo {
+    width: 60px;
+    margin-top: 20px;
+    padding-left: 50px;
+  }
 
   & #logo {
-    margin-top: 30px;
-    font-size: 30px;
+    margin-top: 20px;
+    font-size: 60px;
+    margin-right: 75px;
   }
 `;
 
