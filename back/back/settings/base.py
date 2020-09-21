37129aa709ed436a945c3dbe13e597a8e3a875a6
yehee <yveltal@naver.com>
2020-09-21 16:07:20 +0900

    S03P23D207-9 | email login

--- back/back/settings/base.py
+++ back/back/settings/base.py
@@ -127,7 +127,23 @@ USE_L10N = True
 
 USE_TZ = True
 
+# 이메일로 로그인
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
 SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_UNIQUE_EMAIL = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
