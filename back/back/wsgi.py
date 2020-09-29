476c3fbc507a7c5438d208faee3bc6e8a227114a
홍세진 <hongsj36@naver.com>
2020-09-29 03:47:45 +0000

    Update wsgi.py
    Former-commit-id: 28fbcc1582dda9d36ffeb229a57ca5aae447aa60

--- back/back/wsgi.py
+++ back/back/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import sys
 print(sys.path)
-sys.path.append('/usr/local/lib/python3.7/site-packages')
-sys.path.append('/srv/docker-server')
+sys.path = [
+    '/srv/docker-server',
+    '/usr/local/lib/python37.zip',
+    '/usr/local/lib/python3.7',
+    '/usr/local/lib/python3.7/lib-dynload',
+    '/usr/local/lib/python3.7/site-packages',
+]
+print(sys.path)
 
 import os
 
