7f6a0f2eaba518efcedd8ebfbd356b35e8eba0e4
이윤민 <dbsalsdl5@naver.com>
2020-09-30 00:56:40 +0900

    add 꽃 공공데이터
    Former-commit-id: f1c2186d954c2c7696f608c4b0ce3be33ad7f1c2

--- plant_ai/data_samplecode.py
+++ plant_ai/data_samplecode.py
@@ -2,15 +2,25 @@ from urllib.request import Request, urlopen
 from urllib.parse import urlencode
 from urllib.parse import quote_plus, unquote
 
-url = 'http://openapi.forest.go.kr/openapi/service/cultureInfoService/fStoryOpenAPI'
-# queryParams = '?' + urlencode({ quote_plus('ServiceKey') : 'iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D' })
-# key가 인코딩 되어있어서 디코딩 해주기
+# url = 'http://openapi.forest.go.kr/openapi/service/cultureInfoService/fStoryOpenAPI'
+# # queryParams = '?' + urlencode({ quote_plus('ServiceKey') : 'iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D' })
+# # key가 인코딩 되어있어서 디코딩 해주기
+# decode_key = unquote('iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D')
+# queryParams = '?' + urlencode({ quote_plus('ServiceKey') : decode_key })
+
+# request = Request(url + queryParams)
+# request.get_method = lambda: 'GET'
+# # response_body = urlopen(request).read()
+# # 결과가 인코딩 되어있어서 디코딩 해주기
+# response_body = urlopen(request).read().decode('utf-8')
+# # print(response_body)
+
+
+url = 'http://apis.data.go.kr/1390804/NihhsTodayFlowerInfo/selectTodayFlowerList'
 decode_key = unquote('iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D')
 queryParams = '?' + urlencode({ quote_plus('ServiceKey') : decode_key })
 
 request = Request(url + queryParams)
 request.get_method = lambda: 'GET'
-# response_body = urlopen(request).read()
-# 결과가 인코딩 되어있어서 디코딩 해주기
 response_body = urlopen(request).read().decode('utf-8')
-# print(response_body)
+print(response_body)
--- plant_ai/get_plantdata2.py
+++ plant_ai/get_plantdata2.py
@@ -10,32 +10,15 @@ from time import sleep
 
 ############### print와 sleep이 있어야 500에러 안 뜸. 서버에 너무 많이 계속 접근하면 500에러 떠서 중간에 딜레이 줘야함
 
-
-
-# pageNO = [x for x in range(1,19)] 
-# df = pd.DataFrame(columns=['fslistno', 'fskname', 'fsinhabit', 'fsguide', 'fslifetime', 'imgfilename1', 'imgfilename2'])
-# k = 0
-# for i in range(len(pageNO)):
-#     URL = 'http://apis.data.go.kr/openapi/service/cultureInfoService/fStoryOpenAPI?serviceKey=iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D&type=xml&pageNo='+str(pageNO[i])+'&numOfRows=10&flag=Y'
-    
-#     # if i!=0:
-#     #     sleep(200) 
-#     data = urlopen(URL).read()
-#     soup = BeautifulSoup(data, "html.parser")
-#     print("here!!!!!")
-#     for item in soup.findAll("item"):
-#         imageName = ['', '']
-#         imageURL = 'http://apis.data.go.kr/openapi/service/cultureInfoService/fStoryImgOpenAPI?serviceKey=iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D&searchWrd='+item.fslistno.text
-#         imageData = urlopen(imageURL).read()
-#         imageSoup = BeautifulSoup(imageData, "html.parser")
-#         idx = 0
-#         for image in imageSoup.findAll("item"):
-#             imageName[idx] = 'http://www.forest.go.kr/images/data/down/story/' + image.imgfilename.text
-#             print(item.fslistno.text, ' ', image.imgno.text , ' ', idx, ' ', imageName[idx])
-#             idx = idx + 1
-#             idx = idx % 2
-#             sleep(0.05)
-
-#         df.loc[k] = [item.fslistno.text, item.fskname.text, item.fsinhabit.text, item.fsguide.text, item.fslifetime.text, imageName[0], imageName[1]]
-#         k=k+1
-# df.to_csv('./datasets/plantList.csv',encoding='utf-8-sig')
+pageNO = [x for x in range(1,38)] 
+df = pd.DataFrame(columns=['datano', '꽃 이름', '꽃 영문이름', '꽃말', '사용', '생장'])
+k = 0
+for i in range(len(pageNO)):
+    URL = 'http://apis.data.go.kr/1390804/NihhsTodayFlowerInfo/selectTodayFlowerList?serviceKey=iVBqaTNxcw6XtxmoFOHsio9Tt9VcHg7KJgq8OqkSDuaetVbO6eNnX12yRZE132RsY3wRbahNCICXi0ztwD0j4w%3D%3D&type=xml&pageIndex='+str(pageNO[i])+'&pageUnit=10&flag=Y'
+    data = urlopen(URL).read()
+    soup = BeautifulSoup(data, "html.parser")
+    print("here!!!!!")
+    for result in soup.findAll("result"):
+        df.loc[k] = [result.datano.text, result.flownm.text, result.fengnm.text, result.flowlang.text, result.fuse.text, result.fgrow.text]
+        k=k+1
+df.to_csv('./datasets/flowerList.csv',encoding='utf-8-sig')
