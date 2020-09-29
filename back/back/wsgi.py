a711583ade0fe68f1e0b552c3fbe6cc91a64a89e
홍세진 <hongsj36@naver.com>
2020-09-29 03:22:29 +0000

    Update wsgi.py
    Former-commit-id: dac1374f2d002938bd8036049f95211b4fc3bbca

--- back/back/wsgi.py
+++ back/back/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import sys
 print(sys.path)
 sys.path.append('/usr/local/lib/python3.7/site-packages')
+sys.path.append('/srv/docker-server')
 
 import os
 
