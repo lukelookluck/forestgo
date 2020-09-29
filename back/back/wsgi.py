362d993901405faef080d918d077b845f17755c2
홍세진 <hongsj36@naver.com>
2020-09-29 02:02:42 +0000

    Update wsgi.py
    Former-commit-id: 3d09bac409eaa83d503a3bc37a85d8049c832d4e

--- back/back/wsgi.py
+++ back/back/wsgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+import sys
+print(sys.path)
+sys.path.append('/usr/local/lib/python3.7/site-packages')
+
 import os
 
 from django.core.wsgi import get_wsgi_application
