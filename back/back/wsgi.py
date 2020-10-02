2c48c63f773980196d5732cd9a9fcbdbf064f957
홍세진 <hongsj36@naver.com>
2020-10-02 17:00:24 +0900

    Update requirements.txt
    Former-commit-id: aed4ebdbbfc6393ab874d5968fc36d97bc4ed61a

--- back/back/wsgi.py
+++ back/back/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+from django.core.wsgi import get_wsgi_application
+import os
 import sys
-print(sys.path)
 new_path = [
     '/srv/docker-server',
     '/usr/local/lib/python37.zip',
@@ -17,11 +18,7 @@ new_path = [
     '/usr/local/lib/python3.7/site-packages',
 ]
 sys.path.extend(new_path)
-print(sys.path)
 
-import os
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings.product')
 
