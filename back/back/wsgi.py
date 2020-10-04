61df80f9cbfbefe665299f87565ff0fc9f36f9fb
성예희 <yveltal@naver.com>
2020-10-04 06:31:31 +0000

    Revert "Revert "Merge branch 'back' into 'master'""
    This reverts commit 8da8cc97b3a043fec6304d5e63915df118da9138 [formerly f860681b38d4efd1ab3412399911b3898175668e]
    
    Former-commit-id: de2bcaefb267380407972f4a33b63d8a6920d3ac

--- back/back/wsgi.py
+++ back/back/wsgi.py
@@ -6,7 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-import os, sys
+from django.core.wsgi import get_wsgi_application
+import os
+import sys
 new_path = [
     '/srv/docker-server',
     '/usr/local/lib/python37.zip',
@@ -16,6 +18,5 @@ new_path = [
 ]
 sys.path.extend(new_path)
 
-from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings.product')
 application = get_wsgi_application()
--- back/forestbook/serializers.py
+++ back/forestbook/serializers.py
@@ -2,20 +2,19 @@ from rest_framework import serializers
 from .models import Forestbook, Userbook
 
 
-
 class ForestbookSerializer(serializers.ModelSerializer):
 
-    img = serializers.ImageField(use_url = True)
-    
+    img = serializers.ImageField(use_url=True)
+
     class Meta:
         model = Forestbook
         fields = '__all__'
 
+
 class UserbookSerializer(serializers.ModelSerializer):
 
-    img = serializers.ImageField(use_url = True)
+    img = serializers.ImageField(use_url=True)
 
     class Meta:
         model = Userbook
         fields = '__all__'
-
