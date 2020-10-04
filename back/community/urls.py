95cc253d255a5fdac79f906d698a28fe3444a393
성예희 <yveltal@naver.com>
2020-10-04 06:41:33 +0000

    Revert "Merge branch 'back' into 'master'"
    This reverts merge request !9
    
    Former-commit-id: b25d60102c6ef60e9c6f3ae5faded7dd594d4ce3

--- back/community/urls.py
+++ back/community/urls.py
@@ -7,11 +7,12 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleViewSet.as_view()),
-    path('my_article/', views.UserArticleViewSet.as_view()),
     path('<int:pk>/', views.DetailArticle.as_view()),
     path('comment/', views.ListComment.as_view()),
     path('comment/<int:pk>/', views.DetailComment.as_view()),
     path('article/<int:article_pk>/', views.LikeArticle),
+
+    path('article_like/<int:article_pk>/', views.LikeArticle),
     path('comment/like/<int:comment_pk>/', views.LikeComment),
 
 
