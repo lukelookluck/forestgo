b00cbc3313e2c5cabd728a958e87f5f11a27a5da
yehee <yveltal@naver.com>
2020-10-04 18:12:53 +0900

    added myforest
    Former-commit-id: 654db9a322da41cd2a2d012410b6d381a0b2db42

--- back/forestbook/serializers.py
+++ back/forestbook/serializers.py
@@ -11,10 +11,23 @@ class ForestbookSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class UserbookSerializer(serializers.ModelSerializer):
+# class UserbookSerializer(serializers.ModelSerializer):
 
+#     img = serializers.ImageField(use_url=True)
+
+#     class Meta:
+#         model = Userbook
+#         fields = '__all__'
+
+class UserbookSerializer(serializers.ModelSerializer):
     img = serializers.ImageField(use_url=True)
+    flowername = serializers.SerializerMethodField()
 
     class Meta:
         model = Userbook
         fields = '__all__'
+        read_only_fields = ()
+    
+    def get_flowername(self, obj):
+        return obj.forestbook_id.name
+    
