414a082d71b8ff32aa4c95f081a8da5c55af3871
이윤민 <dbsalsdl5@naver.com>
2020-10-05 19:12:57 +0900

    merge front
    Former-commit-id: 4dcacf94db0668c6ef1233c28cc094940669986e

--- front/src/pages/MyForest/index.jsx
+++ front/src/pages/MyForest/index.jsx
@@ -18,6 +18,7 @@ const MyForest = () => {
   const [fall, setFall] = useState(0);
   const [winter, setWinter] = useState(0);
   const [flag, setFlag] = useState(0);
+  const [articleList, setArticleList] = useState([]);
 
   let season = ""; 
 
@@ -27,8 +28,15 @@ const MyForest = () => {
   let wincnt = 0;
   let flacnt = 0;
 
+  const onClickRedirectPathHandler = (name) => (e) => {
+    window.scrollTo(0, 0);
+    history.push(name);
+  };
+
+
   useEffect(() => {
     refreshList();
+    refreshArticleList();
     if(flowerList.length === 0) {
       console.log("i don't know")
     }
@@ -88,6 +96,25 @@ const MyForest = () => {
       });
   }
 
+  function refreshArticleList() {
+    Axios.get(`${serverUrl}/community/my_article/`, {
+      headers: {
+        Authorization: `JWT ${user.token}`,
+      },
+      params: {
+        user : user.user.id
+      },
+    })
+      .then((response) => {
+        console.log(user.user.id);
+        console.log(response.data);
+        setArticleList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   const handlelogout = () => {
     // 로그아웃
     const url = `${serverUrl}/api/rest-auth/logout/`;
@@ -135,24 +162,27 @@ const MyForest = () => {
           </Paper>
         </Grid>
         
-
         <Grid item xs={12} className="papergrid">
           <Paper variant="outlined">
             <Grid className="title">{user.user.username}님의 식물을 볼 수 있는 계절은</Grid>
-            <Grid item xs={12} className="chart">
+            {flowerList.length === 0
+            ? <Fragment>아직 수집한 식물이 없어요. 식물을 촬영해보세요!</Fragment>
+            : <Grid item xs={12} className="chart">
               { flag === flowerList.length
               ? <Fragment>
                   <SeasonChart
                     SeasonCnt={[spring, summer, fall, winter]}
                     ></SeasonChart>
+
                   <Grid item xs={12}>더운 여름을 잘 이겨내는 사람이군요! 같은 가장 많이 모은 계절에 맞는 멘트 넣기</Grid>
               </Fragment>
               : <Fragment></Fragment>}
             </Grid>
+            }
           </Paper>
         </Grid>
 
-        <Grid item xs={12} className="papergrid" id="lastgrid">
+        <Grid item xs={12} className="papergrid">
           <Paper variant="outlined">
             <Grid className="title">{user.user.username}님의 꽃말은</Grid>
             <Grid container justify="center" alignItems="center">
@@ -162,6 +192,119 @@ const MyForest = () => {
           </Paper>
         </Grid>
 
+        <Grid item xs={12} className="papergrid">
+          <Paper variant="outlined">
+            <Grid container>
+              <Grid className="title_p">{user.user.username}님이 촬영한 사진</Grid>
+              {flowerList.length === 0
+              ? <Fragment></Fragment>
+              : <Grid className="seepic">
+                  <div onClick={onClickRedirectPathHandler("/MyPic")}>+ 더보기</div>
+                </Grid>
+              }
+            </Grid>
+            <Grid container justify="center" alignItems="center" className="picGrid">
+              { flowerList.length <= 0
+              ? <Fragment>식물을 촬영해보세요</Fragment> 
+              : flowerList.length <= 3
+              ? <Grid container justify="center" alignItems="center">
+                  {flowerList.map((pic, index) => (
+                      <Grid item key={index} xs={12 / flowerList.length}>
+                        <img src={pic.img} width="60px" height="60px"></img>
+                      </Grid>
+                  ))}
+                </Grid>
+              : <Grid container justify="center" alignItems="center">
+                  <Grid item xs={4}>
+                    <img src={flowerList[flowerList.length - 1].img} width="60px" height="60px"></img>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <img src={flowerList[flowerList.length - 2].img} width="60px" height="60px"></img>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <img src={flowerList[flowerList.length - 3].img} width="60px" height="60px"></img>
+                  </Grid>
+                </Grid>
+              }
+            </Grid>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} className="papergrid" id="lastgrid">
+          <Paper variant="outlined">
+            <Grid container>
+              <Grid className="title_p">{user.user.username}님이 쓴 글</Grid>
+              {articleList.length === 0
+              ? <Fragment></Fragment>
+              : <Grid>
+                  <div className="seearti" onClick={onClickRedirectPathHandler("/MyArticle")}>+ 더보기</div>
+                </Grid> 
+              }
+            </Grid>
+            <Grid container justify="center" alignItems="center">
+            { articleList.length <= 0
+              ? <Fragment>게시글을 작성해보세요!</Fragment> 
+              : articleList.length <= 3
+              ? <Grid item xs={12}>
+                  {articleList.map((arti, index) => (
+                    <Fragment key={index}>
+                      <Grid container justify="center" alignItems="center">
+                      <Grid item xs={3}>
+                        <Paper className="imgPaper">
+                          <img src={arti.image} width="60px" height="60px"></img>
+                        </Paper>
+                      </Grid>
+                      <Grid item xs={9}>
+                        <div className="titleDiv">{arti.title}</div>
+                        <div className="dateDiv">{arti.created_at.substring(0,10)} 작성</div>
+                      </Grid>
+                    </Grid>
+                    <hr width="350px"></hr>
+                    </Fragment>
+                  ))}
+                </Grid>
+              : <Grid item xs={12}>
+                  <Grid container justify="center" alignItems="center">
+                    <Grid item xs={3}>
+                      <Paper className="imgPaper">
+                        <img src={articleList[articleList.length - 1].image} width="60px" height="60px"></img>
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <div className="titleDiv">{articleList[articleList.length - 1].title}</div>
+                      <div className="dateDiv">{articleList[articleList.length - 1].created_at.substring(0,10)} 작성</div>
+                    </Grid>
+                  </Grid>
+                  <hr width="350px"></hr>
+                  <Grid container justify="center" alignItems="center">
+                    <Grid item xs={3}>
+                      <Paper className="imgPaper">
+                        <img src={articleList[articleList.length - 2].image} width="60px" height="60px"></img>
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <div className="titleDiv">{articleList[articleList.length - 2].title}</div>
+                      <div className="dateDiv">{articleList[articleList.length - 2].created_at.substring(0,10)} 작성</div>
+                    </Grid>
+                  </Grid>
+                  <hr width="350px"></hr>
+                  <Grid container justify="center" alignItems="center">
+                    <Grid item xs={3}>
+                      <Paper className="imgPaper">
+                        <img src={articleList[articleList.length - 3].image} width="60px" height="60px"></img>
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <div className="titleDiv">{articleList[articleList.length - 3].title}</div>
+                      <div className="dateDiv">{articleList[articleList.length - 3].created_at.substring(0,10)} 작성</div>
+                    </Grid>
+                  </Grid>
+                  <hr width="350px"></hr>
+                </Grid>
+              }
+            </Grid>
+          </Paper>
+        </Grid>
 
       </Grid>
     </Wrapper>
--- front/src/pages/MyForest/styles.jsx
+++ front/src/pages/MyForest/styles.jsx
@@ -22,10 +22,6 @@ const Wrapper = styled.div`
         font-size: 20px;
     }
 
-    .habi {
-        font-size: 20px;
-    }
-
     .papergrid {
         margin-top: 10px;
         margin-bottom: 10px;
@@ -51,6 +47,65 @@ const Wrapper = styled.div`
     .root {
         padding-bottom: 30px;
     }
+
+    .picGrid {
+        margin-bottom: 10px;
+    }
+
+    .title_p {
+        text-align: left;
+        font-size: 18px;
+        font-weight: bold;
+        margin-left: 13px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        width: 287px;
+    }
+
+    .seepic {
+        float: right;
+        text-align: right;
+        font-size: 18px;
+        font-weight: bold;
+        margin-top: 10px;
+        color: #509c82;
+    }
+
+    .seearti {
+        float: right;
+        text-align: right;
+        font-size: 18px;
+        font-weight: bold;
+        margin-top: 10px;
+        color: #509c82;
+    }
+
+    .imgPaper {
+        padding: 5px;
+        margin: 5px;
+        margin-left: 20px;
+        width: 70px;
+        height: 70px;
+    }
+    
+    .imgPaper img {
+        padding: 0;
+        margin-top: 5px;
+    }
+
+    .titleDiv {
+        text-align: left;
+        margin-left: 30px;
+        margin-bottom: 20px;
+    }
+
+    & .dateDiv {
+        text-align: right;
+        margin-right: 20px;
+        color: gray;
+    }
+
+    
 `;
 
 export default Wrapper;
